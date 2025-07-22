03c08d14424e592a1beeb913455f83d69e72d6a9
JimmyPOon1 <103594249+JimmyPOon1@users.noreply.github.com>
2025-07-22 20:47:16 +0800

    Update czzy.js

--- js/czzy.js
+++ js/czzy.js
@@ -1,7 +1,7 @@
 const cheerio = createCheerio()
 const CryptoJS = createCryptoJS()
 
-const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15'
 
 let appConfig = {
     ver: 1,
